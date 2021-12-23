f368443d550257480f7674de9b03c9ffcb0b4271
SN963 <72389122+SN963@users.noreply.github.com>
2021-12-23 14:29:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from re import DEBUG
-from tkinter.constants import TRUE
-from flask import flask,jsonify,request
+from flask import Flask, jsonify,request
 import time
-app=flask(__name__)
-@app.route("/bot",method=["POST"])
-
+app = Flask(__name__);
+@app.route("/bot", methods=["POST"])
 def response():
-    query=dict(request.form)['query']
-    result=query+"  "+time.ctime()
-    return jsonify({"response":result})
-
+    query = dict(request.form)['query']
+    res = query + " " + time.ctime()
+    return jsonify({"response" : res})
 if __name__=="__main__":
     app.run(host="0.0.0.0",)
